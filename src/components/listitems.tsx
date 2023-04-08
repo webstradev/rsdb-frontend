b49083fd5c145cb86ad013cde279a0f0066622d4
webstradev <e.s.westra.95@gmail.com>
2023-04-08 22:30:48 +0200

    refactor routes

--- src/components/listitems.tsx
+++ src/components/listitems.tsx
@@ -7,31 +7,38 @@ import NewspaperIcon from "@mui/icons-material/Newspaper";
 import WorkIcon from "@mui/icons-material/Work";
 import VideocamIcon from "@mui/icons-material/Videocam";
 
+const ROUTES = [
+  {
+    path: "/",
+    navBarTitle: "Dashboard",
+    navBarIcon: DashboardIcon,
+  },
+  {
+    path: "/platforms",
+    navBarTitle: "Platforms",
+    navBarIcon: WorkIcon,
+  },
+  {
+    path: "/articles",
+    navBarTitle: "Articles",
+    navBarIcon: NewspaperIcon,
+  },
+  {
+    path: "/projects",
+    navBarTitle: "Projects",
+    navBarIcon: VideocamIcon,
+  },
+];
+
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <WorkIcon />
-      </ListItemIcon>
-      <ListItemText primary="Platforms" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <NewspaperIcon />
-      </ListItemIcon>
-      <ListItemText primary="Articles" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <VideocamIcon />
-      </ListItemIcon>
-      <ListItemText primary="Projects" />
-    </ListItemButton>
+    {ROUTES.map((map) => (
+      <ListItemButton>
+        <ListItemIcon>
+          <map.navBarIcon />
+        </ListItemIcon>
+        <ListItemText primary={map.navBarTitle} />
+      </ListItemButton>
+    ))}
   </React.Fragment>
 );
