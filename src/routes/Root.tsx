89a8d926ebde554b8c5838e25c250838a0e05481
webstradev <e.s.westra.95@gmail.com>
2023-04-10 22:37:51 +0200

    initial api call

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Typography } from "@mui/material";
 
 export const Root: React.FC = () => {
+  useEffect(() => {
+    const checkStatus = async () => {
+      const res = await axios.get(`${import.meta.env.VITE_API_PATH}/health`);
+      console.log(res.status);
+    };
+
+    checkStatus();
+  }, []);
+
   return <Typography component="h2">This is the Root Page!</Typography>;
 };
