8b66c6dc97988f8f8a0a5ef901277cfef9a08cda
webstradev <e.s.westra.95@gmail.com>
2023-04-11 08:13:20 +0200

    add interceptor for authorization token

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
 declare module "axios" {
   export interface AxiosRequestConfig {
@@ -8,9 +8,30 @@ declare module "axios" {
   }
 }
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_PATH,
-  crossDomain: true,
-  xDomain: true,
-  xDomainRequest: true,
-});
+export const getAuthToken = () => localStorage.getItem("token");
+
+const createApi = () => {
+  const api = axios.create({
+    baseURL: import.meta.env.VITE_API_PATH,
+    crossDomain: true,
+    xDomain: true,
+    xDomainRequest: true,
+  });
+
+  // Intercept the request and add the authorization token to the header
+  api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    const authToken = getAuthToken();
+
+    // If no token exists in the browser then we continue normally.
+    if (!authToken) return config;
+
+    // If we have a token we add it as the bearer to the authorization header
+    config.headers["Authorization"] = `Bearer ${authToken}`;
+    return config;
+  });
+
+  return api;
+};
+
+// Create a global API
+export const api = createApi();
