dc7b5c415b21d4af0ee40326c4067f90fe10d632
webstradev <e.s.westra.95@gmail.com>
2023-04-12 22:21:17 +0200

    move api to utils folder

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -7,7 +7,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { api } from "../api";
+import { api } from "util/api";
 
 export const Login: React.FC = () => {
   const [error, setError] = React.useState("");
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { api } from "../api";
+import { api } from "util/api";
 import { Typography, Grid } from "@mui/material";
 
 interface CountsResponse {
