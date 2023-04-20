3dda54a2e068f88fc10f490f68aed80ddc997b61
webstradev <e.s.westra.95@gmail.com>
2023-04-20 15:40:30 +0200

    initial paginated platforms

---
+++ src/components/TablePaginationActions.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+import { IconButton, useTheme, Box } from "@mui/material";
+
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
+}
+
+export const TablePaginationActions = ({
+  count,
+  page,
+  rowsPerPage,
+  onPageChange,
+}: TablePaginationActionsProps) => {
+  const theme = useTheme();
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+  return (
+    <Box sx={{ flexShrink: 0, marginLeft: theme.spacing(2.5) }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+        size="large"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+        size="large"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+        size="large"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+        size="large"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+};
--- src/routes/Platforms/Platforms.tsx
+++ src/routes/Platforms/Platforms.tsx
@@ -1,6 +1,89 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Typography,
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableFooter,
+  TablePagination,
+  TableRow,
+  TableCell,
+  useTheme,
+} from "@mui/material";
+import { useApi } from "util/useApi";
+import { TablePaginationActions } from "components/TablePaginationActions";
 
 export const Platforms: React.FC = () => {
-  return <Typography component="h2">This is the Platforms Page!</Typography>;
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const { data, loading, error } = useApi("get", `/v1/platforms?page=${page}`);
+  const theme = useTheme();
+
+  return (
+    <>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: theme.spacing(3),
+          display: "flex",
+          overflow: "auto",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        <Typography component="h2" variant="h5" color="primary" gutterBottom>
+          Platforms
+        </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Categories</TableCell>
+              <TableCell>Country</TableCell>
+              <TableCell>Website</TableCell>
+              <TableCell>Contacts</TableCell>
+              <TableCell>Articles</TableCell>
+              <TableCell>Projects</TableCell>
+              <TableCell>Source</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data?.platforms &&
+              data.platforms.map((platform: any) => (
+                <TableRow key={platform.id}>
+                  <TableCell>{platform.name}</TableCell>
+                  <TableCell>{platform.categoryString}</TableCell>
+                  <TableCell>{platform.country}</TableCell>
+                  <TableCell>{platform.website}</TableCell>
+                  <TableCell>{platform.contactsCount}</TableCell>
+                  <TableCell>{platform.articlesCount}</TableCell>
+                  <TableCell>{platform.projectsCount}</TableCell>
+                  <TableCell>{platform.source}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                colSpan={8}
+                count={data?.total}
+                rowsPerPage={pageSize}
+                labelRowsPerPage="Rows"
+                page={page}
+                rowsPerPageOptions={[10, 25, 50, 100]}
+                onPageChange={(e, newPage) => setPage(newPage)}
+                onRowsPerPageChange={(e) => {
+                  setPage(0);
+                  setPageSize(+e.target.value);
+                }}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </Paper>
+    </>
+  );
 };
